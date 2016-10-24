f3428e15db57a4952e900026cafe9d7a0deb496b
zhuy <ying1.zhu@intel.com>
2016-10-24 15:08:42 +0800

    first commit

---
+++ Image Gallery/scrpits/showPic.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ying on 16-10-20.
+ */
+
+function showPic(whichPic) {
+    var source = whichPic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+    var text = whichPic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
+}
